51a1a66fbbbef10d0332af9d523537b0a9b15431
akiayu13 <akiayu13@gmail.com>
2022-08-15 19:47:37 +0530

    Video Playing!

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -32,7 +32,7 @@ const MovieDetails = () => {
     axios.get(fetchURL).then((res) => {
       const videos = res.data.results;
       if (res.data.results.length) {
-        videos.map((item) => {
+        videos?.map((item) => {
           if (item.type === "Trailer")
             setVid(`https://www.youtube.com/watch?v=${item.key}`);
         });
